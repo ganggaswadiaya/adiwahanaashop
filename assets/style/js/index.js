09a9571d3b51104598400a9d263ffee241772b04
ganggaswadiaya <ganggaswadiaya@gmail.com>
2024-08-07 10:59:42 +0800

    make form

--- assets/style/js/index.js
+++ assets/style/js/index.js
@@ -5,3 +5,14 @@ const navLink = document.querySelector('.nav-links');
 humberger.addEventListener('click', () => {
   navLink.classList.toggle('active');
 });
+
+// message
+function sendMessage() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  const url = "https://api.whatsapp.com/send?phone=628123605722&text=Hii%20AdiWahana...%0AI'm%20*"+ name +"*%0AMy%20email%20*"+ email +"*%0A%0A*"+ message +"*";
+
+  window.open(url);
+}
