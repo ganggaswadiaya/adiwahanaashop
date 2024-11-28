e7ae598ca82c5a16dbca4ad902c7981ab2539643
ganggaswadiaya <ganggaswadiaya@gmail.com>
2024-11-28 10:28:45 +0800

    add footer

--- assets/style/js/index.js
+++ assets/style/js/index.js
@@ -12,7 +12,7 @@ function sendMessage() {
   const email = document.getElementById('email').value;
   const message = document.getElementById('message').value;
 
-  const url = "https://api.whatsapp.com/send?phone=628123605722&text=Hii%20AdiWahana...%0AI'm%20*"+ name +"*%0AMy%20email%20*"+ email +"*%0A%0A*"+ message +"*";
+  const url = "https://api.whatsapp.com/send?phone=628123605722&text=Hai%20" + name + ",%0A%0ATerima%20kasih%20telah%20menghubungi%20kami!%0ASaya%20Adiwahana%2C%20dan%20saya%20ingin%20mengonfirmasi%20bahwa%20kami%20telah%20menerima%20pesan%20Anda.%0A%0AEmail%20Anda%3A%20" + email + "%0A%0APesan%20Anda%3A%20" + message + "%0A%0AKami%20akan%20segera%20membalas%20pesan%20Anda%20dan%20memberikan%20informasi%20yang%20Anda%20butuhkan.%0A%0ATerima%20kasih%20telah%20menghubungi%20kami%2C%20dan%20kami%20akan%20segera%20menghubungi%20Anda%20kembali!";
 
   window.open(url);
 }
